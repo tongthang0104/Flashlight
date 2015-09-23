f803a24f62d3b627f9a12e033758cb9c08a511c2
Caleb Hicks <calebhicks@gmail.com>
2015-09-23 17:04:36 -0600

    adds sample solution

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    var isOn: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+        if isOn {
+            self.isOn = false
+            
+            self.button.setTitle("On", forState: .Normal)
+            self.button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            self.view.backgroundColor = UIColor.blackColor()
+            
+        } else {
+            self.isOn = true
+            
+            self.button.setTitle("Off", forState: .Normal)
+            self.button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+    
+    }
 
 }
 
